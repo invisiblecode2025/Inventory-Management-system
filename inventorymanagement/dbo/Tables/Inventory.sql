d6c5429942a402e738fe26153ec23540c9e7b72d
Yasser elreedy <yasser.elreedy@ecapital.com>
2025-01-29 16:45:14 +0200

    update database schema

--- inventorymanagement/dbo/Tables/Inventory.sql
+++ inventorymanagement/dbo/Tables/Inventory.sql
@@ -10,6 +10,9 @@
     [LastUpdateDate] DATETIME       NULL,
     [OrderDate]      DATETIME       NULL,
     CONSTRAINT [PK_Inventory] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Inventory_Item] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([Id])
+    CONSTRAINT [FK_Inventory_Item] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([Id]),
+    CONSTRAINT [FK_Inventory_Supplier] FOREIGN KEY ([SupplierId]) REFERENCES [dbo].[Supplier] ([Id])
 );
+
+
 
